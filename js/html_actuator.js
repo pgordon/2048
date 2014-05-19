0568f629f0b97b35aa6ae98e21ec0da54e6053a5
pgordon <mrs.mattstone@gmail.com>
2014-05-18 18:41:48 -0700

    Redid changes to tiles
    tile wording, colours and sizes

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -52,18 +52,18 @@ HTMLActuator.prototype.clearContainer = function (container) {
 
 HTMLActuator.prototype.addTile = function (tile) {
   var valueMap = {
-    2 :    '<Udacity>',
-    4 :    '<Intro CS>',
-    8 :    "skills=['code']",
-    16 :   "skills.add('CSS')",
-    32 :   '</Intro CS>',
-    64 :   '<Job search>',
-    128 :  'getJob(skills)',
-    256 :  '</Job search>',
-    512 :  'if Udacious:',
-    1024 : 'skills.increase()',
-    2048 : 'myJob.advance()',
-    4096 : 'myCareer=myJob'
+    2 :    '1, 2',
+    4 :    'Buckle my shoe',
+    8 :    "3, 4",
+    16 :   "Open the door",
+    32 :   '5, 6',
+    64 :   'Pick up sticks',
+    128 :  '7, 8',
+    256 :  'Lay \'em straight',
+    512 :  '9, 10',
+    1024 : 'The big ',
+    2048 : 'fat hen',
+    4096 : 'you won'
   }
   var self = this;
 
